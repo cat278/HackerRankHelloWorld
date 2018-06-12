dc0406c457bede27817e50bdcbe5dfbff24ae463
divyag <divyag@192.168.43.160>
2018-06-12 23:22:17 +0530

    Hello World Project Done

---
+++ HackerRankHelloWorld/src/HackerRankHelloWorld.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class HackerRankHelloWorld {
+	public static void main(String[] args){
+		//Scanner scan = new Scanner(System.in); 
+		//String inputString = scan.nextLine();
+		//scan.close(); 
+
+		System.out.println("Hello, World.");
+        System.out.println("HackerRank is the best!");
+        System.out.println("Welcome to 30 Days of Code!");
+ 
+    
+	}
+}
+
